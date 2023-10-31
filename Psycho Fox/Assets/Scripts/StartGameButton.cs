7c3966b15785bee003bac6a7bd847833fa9289d7
Vergburger <nicholas.johnson12c@gmail.com>
2023-10-30 22:01:37 -0500

    Stopwatch Add to StartGameButton.cs
    Stopwatch inside of StartGameButton.cs to test for bottlenecks.

--- Psycho Fox/Assets/Scripts/StartGameButton.cs
+++ Psycho Fox/Assets/Scripts/StartGameButton.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Diagnostics;
 
 public class StartGameButton : MonoBehaviour
 {
     public void StartGame()
-    {
+    {   
+        Stopwatch st = new Stopwatch();
+        st.Start();
         SceneManager.LoadScene("Main Scene");
+        st.Stop();
+        UnityEngine.Debug.Log(string.Format
+        ("StartGame took {0} ms to complete", 
+        st.ElapsedMilliseconds));
     }
 }
