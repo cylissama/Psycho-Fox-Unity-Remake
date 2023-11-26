9f36cb0c2505bb50e3b5d0c95c478e13372481a4
Vergburger <nicholas.johnson12c@gmail.com>
2023-11-26 00:49:30 -0600

    progresss

---
+++ Psycho Fox/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class CanvasController : MonoBehaviour
+{
+    public GameObject loginPanel;
+    public GameObject registrationPanel;
+    public GameObject mainPanel;
+    public GameObject scoreboardPanel;
+
+    public InputField loginUsernameInput;
+    public InputField loginPasswordInput;
+    public InputField registerUsernameInput;
+    public InputField registerPasswordInput;
+    public Text errorMessageText;
+    public Text loggedInUsernameText;
+
+    private string loggedInUsername;
+    private bool isLoggedIn = false;
+    private List<UserData> scoreboardData = new List<UserData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowInitialPanel(); // Show the initial panel when the game starts
+    }
+
+    /*
+    // Function to show the initial panel
+    void ShowInitialPanel()
+    {
+        if (isLoggedIn)
+        {
+            ShowMainPanel();
+        }
+        else
+        {
+            ShowLoginPanel();
+        }
+    }
+    */
+
+    // ... (other functions remain the same)
+
+    /*
+    // Function to handle register button click (on registration panel)
+    public void RegisterButtonOnRegistrationPanelClicked()
+    {
+        string username = registerUsernameInput.text;
+        string password = registerPasswordInput.text;
+
+        // Register user (replace this with your registration logic)
+        RegisterUser(username, password);
+        isLoggedIn = true;
+        loggedInUsername = username;
+        ShowMainPanel();
+    }
+
+    // Function to switch from login panel to registration panel
+    public void SwitchToRegistrationPanel()
+    {
+        ShowRegistrationPanel();
+    }
+
+    // Function to switch from registration panel to login panel
+    public void SwitchToLoginPanel()
+    {
+        ShowLoginPanel();
+    }
+
+    // ... (other functions remain the same)
+    */
+}
