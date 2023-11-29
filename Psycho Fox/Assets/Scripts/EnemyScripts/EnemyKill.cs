f57141f40b2d976b0a3de21402f20b96db5f3e96
Matthew Polak <polak.matthew2003@gmail.com>
2023-11-29 17:03:41 -0600

    punching working

---
+++ Psycho Fox/Assets/Scripts/EnemyScripts/EnemyKill.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKill : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.CompareTag("Enemy")) {
+            Destroy(col.gameObject);
+        }
+    }
+}
